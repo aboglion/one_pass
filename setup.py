b8ad1a6fc3d92288b298762c6ca3a1f0bf798991
aboglion <aboglion@gmail.com>
2024-01-25 12:17:51 +0200

    JH

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ONE_PASS',
-    version='0.1.02',
+    version='0.1.03',
     packages=find_packages(),
     description='To manage private variables with only one password, a key and the name of the encrypted file. To protect sensitive data from being uploaded to GitHub',
     author='U.N.S',
